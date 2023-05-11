26c503e42c1cfd564d8569e63d2d7380adc6659a
Francis Yvan Jubera <francisyvanjubera@gmail.com>
2023-05-11 10:00:01 +0800

    set-up a basic express server application

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-const a = 5, b = 3;
+const express = require('express');
+const app = express();
 
+app.get("/", (req, res) => {
+    res.send("<h1 style='color: blue; font-size: 3rem'>Homepage</h1>");
+});
 
-console.log("This is just a test: ", a + b);
+app.get("/about", (req, res) => {
+    res.send("<h1 style='color: green; font-size: 3rem'>About Page</h1>");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("<h1 style='color: orange; font-size: 3rem'>Contact Page</h1>");
+});
+
+app.all("*", (req, res) => {
+    res.status(404);
+    res.send("<h1 style='color: red; font-size: 3rem'>The page your requesting does not exist!</h1>");
+});
+
+app.listen(5000, () => {
+    console.log("Server online, listening on port 5000");
+});
